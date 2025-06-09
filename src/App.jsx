b455723508565534ff0f3448a84a0c4fbe5ceadf
Sonlong1012 <103424085+Sonlong1012@users.noreply.github.com>
2025-06-09 15:04:44 +0700

    cap nhat them chuc nang

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,42 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import {Toaster} from "react-hot-toast";
+import { useAppContext } from "./context/Appcontext";
+import Login from "./components/Login";
+import AllProducts from "./pages/AllProducts";
+import ProductCategory from "./pages/ProductCategory";
+import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import AddAddress from "./pages/AddAddress";
+import MyOrders from "./pages/MyOrders";
+import SellerLogin from "./components/seller/SellerLogin";
+import SellerLayout from "./pages/seller/SellerLayout";
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin, isSeller} = useAppContext()
   return (
-    <div>
+    <div className='text-default min-h-screen text-gray-700 bg-white'>
      {isSellerPath ? null: <Navbar/> }
-
+     {showUserLogin ? <Login/> : null}
       <Toaster/>
 
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/>} />
+          <Route path='/products/:category/:id' element={<ProductDetails/>} />
+          <Route path='/products' element={<AllProducts/>} />
+          <Route path='/products/:category' element={<ProductCategory/>} />
+          <Route path='/cart' element={<Cart/>} />
+          <Route path='/add-address' element={<AddAddress/>} />
+          <Route path='/my-orders' element={<MyOrders/>} />
+          <Route path='/seller' element={isSeller ? <SellerLayout/> : <SellerLogin/>}>
+
+          </Route>
         </Routes>
       </div>
+      {!isSellerPath && <Footer/>}
     </div>
   )
 }
--- src/context/Appcontext.jsx
+++ src/context/Appcontext.jsx
@@ -7,14 +7,15 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({children})=>{
 
-    const currency = import.meta.VITE_CURRENCY;
+    const currency = import.meta.env.VITE_CURRENCY;
     const navigate = useNavigate();
     const [user, setUser] = useState(null)  // false logout, true login
     const [isSeller, setIsSeller] = useState(false)
-    const [showUserLogin, setShowUserLogin] = useState(false)
+    const [showUserLogin, setShowUserLogin] = useState(false);
     const [products, setProducts] = useState([])
 
     const [cartItems, setCartItems] = useState({})
+    const [searchQuery, setSearchQuery] = useState({})
 
     // Fetch All Products
     const fetchProducts = async()=> {
@@ -55,11 +56,34 @@ export const AppContextProvider = ({children})=>{
     }
 
 
+    // Get Cart Item Count
+    const getCartCount = ()=> {
+        let totalCount = 0;
+        for(const item in cartItems){
+            totalCount += cartItems[item];
+        }
+        return totalCount;
+    }
+
+    // Get Cart Total Amount 
+    const getCartAmount = () =>{
+        let totalAmount = 0;
+        for (const items in cartItems){
+            let itemInfo = products.find((product)=> product._id === items);
+            if(cartItems[items] > 0){
+                totalAmount += itemInfo.offerPrice * cartItems[items]
+            }
+        }
+        return Math.floor(totalAmount * 100) / 100;
+    }
+
+
+
     useEffect(()=>{
         fetchProducts()
     },[])
 
-    const value = {navigate, user, setUser,setIsSeller,isSeller, showUserLogin,setShowUserLogin, products,currency, addToCart,UpdateCartItem,removeFromCart,cartItems}
+    const value = {navigate, user, setUser,setIsSeller,isSeller, showUserLogin,setShowUserLogin, products,currency, addToCart,UpdateCartItem,removeFromCart,cartItems,searchQuery,setSearchQuery,getCartAmount,getCartCount}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
